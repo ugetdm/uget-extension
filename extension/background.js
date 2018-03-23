4103443c988fc022fbb2ad46784b00d3cacfb76c
Gobinath <slgobinath@gmail.com>
2018-03-22 21:35:44 -0400

    Fix min file size not considered

--- extension/background.js
+++ extension/background.js
@@ -358,7 +358,7 @@ function setDownloadHooks() {
                 disposition = details.responseHeaders[i].value;
                 if (disposition.lastIndexOf('filename') != -1) {
                     var found = disposition.match(/filename[^;=\n]*\*=UTF-8''((['"]).*?\2|[^;\n]*)/);
-                    if(found) {
+                    if (found) {
                         message.FileName = decodeURI(found[1]);
                     } else {
                         message.FileName = disposition.match(/filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/)[1];
@@ -733,16 +733,16 @@ function isWhiteListedContent(contentType) {
     whiteListed = false;
     // Test the content type
     if (contentType) {
-        if (/\b(?:application\/|video\/|audio\/)\b/.test(contentType)) {
-            whiteListed = true;
-        } else {
-            for (var keyword of mimeToInterrupt) {
-                if (contentType.includes(keyword)) {
-                    whiteListed = true;
-                    break;
-                }
+        // if (/\b(?:application\/|video\/|audio\/)\b/.test(contentType)) {
+        //     whiteListed = true;
+        // } else {
+        for (var keyword of mimeToInterrupt) {
+            if (contentType.includes(keyword)) {
+                whiteListed = true;
+                break;
             }
         }
+        // }
     }
     return whiteListed;
 }
